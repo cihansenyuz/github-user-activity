c0e1460845bedaaabf6f908b09556b0022602a77
cihansenyuz <cihansenyuz@hotmail.com>
2025-04-08 12:42:46 +0300

    add terminal arguments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
 #include "inc/application.hpp"
 
-int main(){
-    Application app("cihansenyuz");
+int main(int argc, char **argv){
+    if(argc == 1){
+        std::cout << "re-run the app providing a github username"
+        << "\ne.g. $ ./github_user_activity_fetcher cihansenyuz" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string username_to_be_fetched = argv[1];
+    
+    Application app(username_to_be_fetched);
     app.PrintEventStats();
     
-    return 0;
+    return EXIT_SUCCESS;
 }
